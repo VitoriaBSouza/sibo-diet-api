35299a5e2f1b9b2078243430814b63092c8adbf4
Vitoria Barbosa Souza <vitoria.barbosa@DTESMP1626019.local>
2026-06-11 00:20:59 +0200

    created prisma table

--- src/app.py
+++ src/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 
+from src.db.client import db
+
 from src.routes.health import health_bp
 from src.routes.user import user_bp
 from src.routes.root import root_bp
 
 def create_app():
     app = Flask(__name__)
+
+    if not db.is_connected():
+        db.connect()
 
     app.register_blueprint(root_bp)
     app.register_blueprint(health_bp)
